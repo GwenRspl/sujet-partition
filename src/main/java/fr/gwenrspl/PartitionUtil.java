6e0cb3b09c48816e7b83e68a7e61d816eb974949
GwenRspl <gwenaelle.rispal@gmail.com>
2020-04-09 18:05:02 +0200

    add javadoc & refactor code

--- src/main/java/fr/gwenrspl/PartitionUtil.java
+++ src/main/java/fr/gwenrspl/PartitionUtil.java
@@ -4,12 +4,27 @@ package fr.gwenrspl;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The class containing the partition method
+ *
+ * @author Gwen Rspl
+ */
 public final class PartitionUtil {
-    //TODO javadoc
 
+    /**
+     * Private constructor to prevent from instanciating this class
+     */
     private PartitionUtil() {
     }
 
+    /**
+     * This method takes a list of Integers and a size as parameters, and partition the list into sublists
+     *
+     * @param list The list of Integers to partition into sublists
+     * @param size The maximum size of the sublists
+     * @return A list of appropriately-sized sublists of Integers
+     * @throws IllegalArgumentException if the list given is null or if the size is less than 1
+     */
     public static List<List<Integer>> partition(final List<Integer> list, final int size) {
         if (list == null) {
             throw new IllegalArgumentException("The list parameter cannot be null.");
@@ -19,30 +34,23 @@ public final class PartitionUtil {
         }
 
         final List<List<Integer>> partitionedList = new ArrayList<>();
-        if (list.isEmpty()) {
-            return partitionedList;
+
+        if (!list.isEmpty()) {
+
+            final int numberOfFullLists = list.size() / size;
+            int currentIndex = 0;
+
+            for (int i = 0; i < numberOfFullLists; i++) {
+                partitionedList.add(list.subList(currentIndex, currentIndex + size));
+                currentIndex += size;
+            }
+
+            if (list.size() % size != 0) {
+                partitionedList.add(list.subList(currentIndex, list.size()));
+            }
         }
 
-        final int numberOfFullLists = list.size() / size;
-        int index = 0;
-        for (int i = 0; i < numberOfFullLists; i++) {
-            final List<Integer> secondDimensionList = generateSecondDimensionList(index, index + size, list);
-            partitionedList.add(secondDimensionList);
-            index += size;
-        }
-
-        if (list.size() % size != 0) {
-            final List<Integer> finalSecondDimensionList = generateSecondDimensionList(index, list.size(), list);
-            partitionedList.add(finalSecondDimensionList);
-        }
         return partitionedList;
     }
 
-    private static List<Integer> generateSecondDimensionList(final int startIndex, final int endIndex, final List<Integer> list) {
-        final List<Integer> secondDimensionList = new ArrayList<>();
-        for (int i = startIndex; i < endIndex; i++) {
-            secondDimensionList.add(list.get(i));
-        }
-        return secondDimensionList;
-    }
 }
